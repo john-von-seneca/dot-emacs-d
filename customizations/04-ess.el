820fcd9e4d40fde86cadb8c39489db5ba5c94d17
jvs <john.von.seneca@gmail.com>
2015-11-30 05:47:26 +0530

    R: cstmztns

---
+++ customizations/04-ess.el
@@ -0,0 +1,10 @@
+;ess-mode configuration
+(setq ess-ask-for-ess-directory nil)
+(setq inferior-R-program-name "/usr/local/bin/R")
+(setq ess-local-process-name "R")
+(setq ansi-color-for-comint-mode 'filter)
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+(setq ess-eval-visibly-p nil)
+(require 'ess-site)
