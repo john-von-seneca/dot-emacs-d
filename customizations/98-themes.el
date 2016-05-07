093584265ddb21b6a4fc086351a64990230185ce
jvs <john.von.seneca@gmail.com>
2016-05-07 11:44:40 +0530

    color: leuven

--- customizations/98-themes.el
+++ customizations/98-themes.el
@@ -2,9 +2,12 @@
 
 ;; ;; Theme
 ;; ;; https://github.com/bbatsov/zenburn-emacs
-(load-theme 'zenburn t)
+;; (load-theme 'zenburn t)
 ;; (load-theme 'leuven)
-(set-cursor-color "firebrick")
+(setq prelude-theme 'leuven)
+
+;; (set-cursor-color "firebrick")
+(set-cursor-color "#21BDFF") ;; leuven default
 
 ;; (set-background-color "#3F3F3F")
 ;; (set-background-color "black")
@@ -13,3 +16,5 @@
 ;; ;; Font
 ;; ;; https://www.mozilla.org/en-US/styleguide/products/firefox-os/typeface/#download-primary
 ;; (set-frame-font "Fira Mono OT-14" nil t)
+
+
