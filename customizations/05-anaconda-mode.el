588e7e87afc344eb1a6e5eace456bca8daba9a19
jvs <john.von.seneca@gmail.com>
2016-06-21 09:15:05 +0530

    anaconda mode for python

---
+++ customizations/05-anaconda-mode.el
@@ -0,0 +1,9 @@
+
+
+;; (add-hook 'python-mode-hook 'anaconda-mode)
+;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+
+;; (eval-after-load "company"
+;;  '(add-to-list 'company-backends 'company-anaconda))
+;; (eval-after-load "company"
+;;  '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
