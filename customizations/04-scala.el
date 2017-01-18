54eaae4f588e9fe492b5763ff9d8d8d2f5061a33
bebee <balasanjeevi.b@olacabs.com>
2017-01-18 18:23:33 +0530

    scala

--- customizations/04-scala.el
+++ customizations/04-scala.el
@@ -8,3 +8,74 @@
 ;; if you're not using the standard scala mode.
 ;;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 ;;(setq exec-path (append exec-path (list "/usr/local/bin/" )))
+
+;;We have "sbt" and "scala" in /usr/local/bin so we add this path to the PATH environment
+(setq exec-path (append exec-path '("/usr/local/bin")))
+(setq exec-path (append exec-path '("/usr/local/sbin")))
+;; why is this shite needed?
+(setenv "PATH" (shell-command-to-string "/bin/bash -c 'echo -n $PATH'"))
+
+(require 'scala-mode)
+(require 'ensime)
+
+;; (autoload 'scala-mode "scala-mode2")
+(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.scala$" . ensime-mode))
+
+(add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.scala\\'" . ensime-mode))
+
+
+;; (setq ensime-startup-snapshot-notification nil)
+;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;; (define-key
+;;   scala-mode-map
+;;   (kbd "s-/")
+;;   'comment-or-uncomment-region)
+
+
+(setq ensime-startup-snapshot-notification nil)
+(setq scala-prettify-symbols
+	  '(
+		("=>" . ?⇒)
+		("<-" . ?←)
+		("->" . ?→)
+		("undefined" . ?⊥)
+		("&&" . ?∧)
+		("||" . ?∨)
+		("<<<" . ?⋘)
+		(">>>" . ?⋙)
+		("++" . ?⧺)
+		("any" . ?∃)
+		("all" . ?∀)
+		("traverse" . ?↦)
+		("map" . ?∘)
+		("lambda" . ?λ)
+		("alpha" . ?α)
+		("beta" . ?β)
+		("Unit" . ?∅)
+		))
+
+;; (add-hook 'scala-mode-hook
+;; 		  (lambda ()
+;; 			(ensime-scala-mode-hook)
+;; 			(setq prettify-symbols-alist scala-prettify-symbols)
+;; 			(prettify-symbols-mode)
+;; 			(define-key scala-mode-map (kbd "C-x M-e") 'ensime-fully-reload)
+;; 			))
+
+(defun ensime-fully-reload ()
+  "reload ensime"
+  (interactive)
+  (ensime-shutdown)
+  (ensime))
+
+
+;; (global-unset-key (kbd "s-."))
+;; (global-set-key (kbd "s-.") 'ensime-edit-definition)
+;; (ensime-edit-definition ARG &optional WHERE)
+
+;; (global-unset-key (kbd "s-,"))
+;; (global-set-key (kbd "s-,") 'ensime-pop-find-definition-stack)
+;; (ensime-pop-find-definition-stack)
