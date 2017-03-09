6c80ae9bc8e071b26d9ed862d2c1fe6a9961c666
bebee <balasanjeevi.b@olacabs.com>
2017-03-09 11:06:25 +0530

    global: enable icy-mode

--- customizations/00-global.el
+++ customizations/00-global.el
@@ -90,5 +90,10 @@
 (setq-default tab-stop-list (list 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108))
 
 
+(icy-mode 1)
+
+
 ;; (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
 (provide '00-global)
+
+
